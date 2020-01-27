0c938e25da7a54e7d63544832e461a77e4396dcd
Vikas Shukla <shuk.vk@gmail.com>
2020-01-27 13:45:02 -0800

    Adds or equal to on less than test

--- database-mysql/script.test.js
+++ database-mysql/script.test.js
@@ -3,5 +3,5 @@ const scriptFunction = require('./script.js');
 test ('function returns number between 1 and 30 (inclusive)', () => {
   let randMinDays = scriptFunction.getRandomMinDays();
   expect(randMinDays).toBeGreaterThanOrEqual(1);
-  expect(randMinDays).toBeLessThan(30);
+  expect(randMinDays).toBeLessThanOrEqual(30);
 });
