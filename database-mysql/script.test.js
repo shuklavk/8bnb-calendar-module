4a05a912af7dd97f1e6944792548a9b79fdf8922
Vikas Shukla <shuk.vk@gmail.com>
2020-01-28 03:05:11 -0800

    Deletes script.test.js

--- database-mysql/script.test.js
+++
@@ -1,7 +0,0 @@
-const scriptFunction = require('./script.js');
-
-test ('function returns number between 1 and 30 (inclusive)', () => {
-  let randMinDays = scriptFunction.getRandomMinDays();
-  expect(randMinDays).toBeGreaterThanOrEqual(1);
-  expect(randMinDays).toBeLessThanOrEqual(30);
-});
