27358bbe806ac45fd5dd8cb27ad043200ee15a70
Vikas Shukla <shuk.vk@gmail.com>
2020-01-29 14:51:38 -0800

    Adds front and back button with functionality

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Row from './Row.jsx';
 import DaysOfWeek from './DaysOfWeek.jsx';
 import Header from './Header.jsx';
+import BackButton from './BackButton.jsx';
+import ForwardButton from './ForwardButton.jsx';
 import { addMonths, subMonths } from 'date-fns';
 
 
@@ -9,23 +11,40 @@ class Calendar extends React.Component {
   constructor(props) {
     super(props);
     this.state = ({
-      currMonth: new Date(), // RANDOM STATE TO AVOID ESLINT ERROR IGNORE FOR NOW
+      currMonth: new Date(),
     });
+    this.onForwardClick = this.onForwardClick.bind(this);
+    this.onBackClick = this.onBackClick.bind(this);
+  }
+
+  // Everytime someone clicks the forward arrow key, the current month increases by one
+  onForwardClick() {
+    this.setState((prevState) => ({
+      currMonth: addMonths(prevState.currMonth, 1),
+    }));
+  }
+
+  // Everytime someone clicks the back arrow key, the current month decreases by one
+  onBackClick() {
+    this.setState((prevState) => ({
+      currMonth: subMonths(prevState.currMonth, 1),
+    }));
   }
 
   render() {
     const { currMonth } = this.state;
     const nextMonth = addMonths(currMonth, 1);
-    console.log(nextMonth);
     return (
       <div className="overallComponent" style={{ width: '632px' }}>
         <div className="calendarDiv">
+          <BackButton onBackClick={this.onBackClick} />
           <Header currMonth={currMonth} />
           <DaysOfWeek />
           <Row />
         </div>
 
         <div className="calendarDiv">
+          <ForwardButton currMonth={currMonth} onForwardClick={this.onForwardClick} />
           <Header currMonth={nextMonth} />
           <DaysOfWeek />
           <Row />
