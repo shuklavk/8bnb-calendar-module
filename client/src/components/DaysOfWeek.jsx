6228d46a09f630b53a6e1e602e955b5a774e5354
Vikas Shukla <shuk.vk@gmail.com>
2020-01-29 11:07:28 -0800

    Adds Comment to DaysOfWeek Component

--- client/src/components/DaysOfWeek.jsx
+++ client/src/components/DaysOfWeek.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 const DaysOfWeek = () => {
   const DaysOfWeekArr = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
+  // Create List Element for each day of the week and store in a new array
   const DaysOfWeekListArr = DaysOfWeekArr.map((ele) => (
     <li className="day" style={{ width: '40px' }} key={ele}>
       <small>{ele}</small>
