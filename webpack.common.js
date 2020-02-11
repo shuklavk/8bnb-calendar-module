0be67045d869ccc5500417c0daebc94744d665f8
Vikas Shukla <shuk.vk@gmail.com>
2020-02-10 20:52:04 -0800

    webpack

---
+++ webpack.common.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, 'client/');
+const DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  "mode": "development",
+  "entry": `${SRC_DIR}/index.jsx`,
+  "output": {
+    "path": DIST_DIR,
+    "filename": "bundle.js"
+  },
+  "module": {
+    "rules": [
+      {
+        "test": /\.(js|jsx)$/,
+        "exclude": /node_modules/,
+        "use": {
+          "loader": "babel-loader",
+          "options": {
+            "presets": [
+              "@babel/preset-env",
+              "@babel/react"
+            ]
+          }
+        }
+      },
+      {
+        "test": /\.css$/,
+        "use": [
+          "style-loader",
+          "css-loader"
+        ]
+      }
+    ]
+  }
+}
